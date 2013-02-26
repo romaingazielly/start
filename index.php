ae9db8cc419bc83724c849282000a8a18fa37ebb
romaingazielly <ganondorf_00@hotmail.fr>
2013-02-26 21:55:37 +0100

    1er commit

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!doctype html>
+<html lang="fr">
+<head>
+<title></title>
+<base href="" />
+<link rel="icon" type="image/png" href="favicon.ico" />
+<meta name="description" content=""/>
+<meta name="keywords" content=""/>
+<meta name="author" content=""/>
+<meta name="robots" content="index, follow, noydir, noodp, all"/>
+<meta charset="UTF-8"/>
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<![endif]-->
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="js/jquery.js" type="text/javascript"></script>
+</head>
+<body>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*----------------------------------
+*							
+*	Version: 1.0				  
+*	Date: 11/02/2013			  
+*	Author: Romain GAZIELLY	  
+*	Mail: romain.gazielly@gmail.com
+*	Website: www.romaingazielly.com
+*								  
+*----------------------------------*/
+
+/*-------------------
+	Code
+-------------------*/
